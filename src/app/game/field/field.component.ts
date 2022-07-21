73740629e52f1f5c77094ca9d248d974f897a0eb
mx <m.obrubov@yandex.ru>
2022-07-21 23:51:12 +0200

    created field node

--- src/app/game/field/field.component.ts
+++ src/app/game/field/field.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+declare var Snap: any;
+// declare var mina: any;
+
 
 @Component({
   selector: 'game-field',
@@ -9,7 +12,18 @@ export class FieldComponent implements OnInit {
 
   constructor() { }
 
+  public size = {
+    width: 24,
+    height: 17,
+  }
+  public CELL_SIZE = 35;
+
+  private ctxSelector = "#field-ctx";
+
   ngOnInit(): void {
+    const s = Snap(this.ctxSelector);
+
+    const c = s.circle(50, 50, 100);
   }
 
 }
